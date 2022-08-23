6dfdc5c9c59326d732b7b08462139ca915cd5934
Cleber B Luiz <luiz.cleber@gmail.com>
2022-08-23 15:02:33 -0300

    exceptions e logger impl

--- src/main/java/com/cleberb/gamesapp/resource/GameResource.java
+++ src/main/java/com/cleberb/gamesapp/resource/GameResource.java
@@ -1,5 +1,6 @@
 package com.cleberb.gamesapp.resource;
 
+import com.cleberb.gamesapp.exceptions.ResourceNotFoundException;
 import com.cleberb.gamesapp.model.Game;
 import com.cleberb.gamesapp.repository.GameRepository;
 import org.springframework.beans.BeanUtils;
@@ -8,33 +9,42 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/api")
 public class GameResource {
 
+    private Logger logger = Logger.getLogger(GameResource.class.getName());
+
     @Autowired
     private GameRepository gameRepository;
 
     @GetMapping("/games")
-    public List<Game> listar(){
+    public List<Game> findAll(){
+        logger.info("Finding all games!");
         return gameRepository.findAll();
     }
 
     @GetMapping("/games/{id}")
-    public Game listarJogoPorId(@PathVariable Long id){
-        return gameRepository.findById(id).get();
+    public Game findById(@PathVariable Long id){
+        logger.info("Finding one game!");
+        return gameRepository.findById(id).
+                orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
     }
 
     @PostMapping("/games")
     @ResponseStatus(HttpStatus.CREATED)
     public Game save(@RequestBody Game game){
+        logger.info("Saving one game!");
         return gameRepository.save(game);
     }
 
     @PutMapping("/games/{id}")
     public Game update(@PathVariable Long id, @RequestBody Game game){
-        Game gameAtual = gameRepository.findById(id).get();
+        logger.info("Updating one game!");
+        Game gameAtual = gameRepository.findById(id).
+                orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
         BeanUtils.copyProperties(game, gameAtual, "id");
         return gameRepository.save(gameAtual);
     }
@@ -42,6 +52,9 @@ public class GameResource {
     @DeleteMapping("/games/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id){
-        gameRepository.deleteById(id);
+        logger.info("Deleting one game!");
+        var entity = gameRepository.findById(id).
+                orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+        gameRepository.delete(entity);
     }
 }
